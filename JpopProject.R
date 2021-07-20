9707559b84fb851fbe5e7af7e5b6a982b6240975
e-paj <84873455+e-paj@users.noreply.github.com>
2021-07-20 15:12:57 -0600

    Update JpopProject.R

--- JpopProject.R
+++ JpopProject.R
@@ -13,9 +13,16 @@ adf.test(J_pop) #Since the p-value is less than 0.05, we can reject the null tha
 pp.test(J_pop) # since the p-value is less than 0.05, we can reject the null that
 # it is non-stationary
 
-# we can see from the plot that there seems to be a trend in the data
-# it is going down.
+# we can see from the plot that there seems to be a decreasing trend in the data
 # Hence, de-trend this first using linear regression
-time <- time(J_pop)
-score.lin <- lm(J_pop~time)
-plot(score.lin,type='l')
+Time <- time(J_pop)
+score.lin <- lm(J_pop~Time)
+par(mfrow=c(2,1))
+plot(resid(score.lin),type='l', main="Detrended")
+plot(diff(J_pop), type='l', main='First Difference')
+
+# to verify that our data has become stationary, take ACF plots
+par(mfrow=c(3,1))
+acf(J_pop, lag.max=30, main="J_pop")
+acf(resid(score.lin), lag.max=30, main='detrended')
+acf(diff(J_pop), lag.max=30, main="first difference")

1b420c79a9ed5b6d11a9ac4e0143d60db7ef3775
e-paj <84873455+e-paj@users.noreply.github.com>
2021-07-22 15:39:03 -0600

    Update JpopProject.R

--- JpopProject.R
+++ JpopProject.R
@@ -17,6 +17,10 @@ pp.test(J_pop) # since the p-value is less than 0.05, we can reject the null tha
 # Hence, de-trend this first using linear regression
 Time <- time(J_pop)
 score.lin <- lm(J_pop~Time)
+plot(J_pop, type='l')
+abline(score.lin, col='red')
+summary(score.lin)
+
 par(mfrow=c(2,1))
 plot(resid(score.lin),type='l', main="Detrended")
 plot(diff(J_pop), type='l', main='First Difference')
@@ -26,3 +30,27 @@ par(mfrow=c(3,1))
 acf(J_pop, lag.max=30, main="J_pop")
 acf(resid(score.lin), lag.max=30, main='detrended')
 acf(diff(J_pop), lag.max=30, main="first difference")
+
+# look at residuals of linear fit
+lin.resid.score <- resid(score.lin)
+plot(lin.resid.score, type='l')
+abline(h=0, col='gray')
+acf(lin.resid.score,lag.max=30)
+pacf(lin.resid.score,lag.max=length(lin.resid.score))
+
+ar(lin.resid.score,aic=T)
+
+## Try some ARIMA models for the residuals of the first linear model
+lin.model1 <- Arima(lin.resid.score,order=c(1,0,0))
+lin.model2 <- Arima(lin.resid.score,order=c(0,0,1))
+lin.model3 <- Arima(lin.resid.score,order=c(1,0,1))
+summary(lin.model1)
+summary(lin.model2)
+summary(lin.model3)
+
+lin.model4 <- Arima(lin.resid.score,order=c(2,0,0))
+lin.model5 <- Arima(lin.resid.score,order=c(0,0,2))
+lin.model6 <- Arima(lin.resid.score,order=c(2,0,2))
+summary(lin.model4)
+summary(lin.model5)
+summary(lin.model6)
